d57a9a77dc623bf51b4fe385ede953b6d3e94494
Dimitriy Yevchenko <92580268+dimitriyechinho22@users.noreply.github.com>
2023-02-20 21:46:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,69 @@
+import osmnx as ox
+import geopandas as gpd
 import pandas as pd
 import sqlite3
-from datetime import datetime
+import datetime
 
+city = "Lviv, Ukraine"
+
+city_boundary = ox.geometries_from_place(city, tags={"boundary": "administrative"})
+
+buildings = ox.geometries_from_place(city, tags={"building": True})
+
+buildings = gpd.sjoin(buildings, city_boundary, how="left", op="within")
+
+numbers = []
+streets = []
+areas = []
+
+for i, row in buildings.iterrows():
+    number = row.get("addr:housenumber")
+    if type(number) == float:
+        number = 'Unknown'
+    numbers.append(number)
+
+    street = row.get("addr:street")
+    if type(street) == float:
+        street = 'Unknown'
+    streets.append(street)
+
+    area = row.get("name_right")
+    if type(area) == float:
+        area = 'Unknown'
+    areas.append(area)
+
+data = {"Number of Building": numbers, "Street": streets, "Area": areas}
+df = pd.DataFrame(data)
+
+mask = df.apply(lambda row: 'Unknown' in row.values, axis=1)
+
+df = df[~mask]
 conn = sqlite3.connect('identifier.sqlite')
 c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS streets
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-             name TEXT,
-             last_name TEXT,
-             street TEXT,
-             time DATETIME,
-            )''')
-conn.commit()
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+try:
+    c.execute('''CREATE TABLE users2 (id INTEGER PRIMARY KEY, Name TEXT, 
+              Last_Name TEXT, 
+              Street TEXT, 
+              Building_Number TEXT, 
+              Area TEXT, 
+              Time DATETIME)''')
+except sqlite3.OperationalError:
+    pass
 
-
-def convert_file_data_to_dataframe(filename):
-    """
-    Функція приймає аргументом шлях до файлу (або назву файлу), редагує дані та повертає pandas Dataframe.
-    :param filename: Назва файлу
-    :return: pandas DataFrame.
-    """
-    with open(filename, "r", encoding="utf-8") as file:
-        data_as_list = file.readlines()
-        data_as_list = [i.rstrip().lstrip().replace("\t", " ")
-                        for i in data_as_list]
-        for elem in range(len(data_as_list)):
-            data_as_list[elem] = [' '.join(data_as_list[elem].split()[:-2]), ' '.join(data_as_list[elem].split()[-2:])]
-
-        main_dataframe = pd.DataFrame(data_as_list, columns=("Street", "Area"))
-        return main_dataframe
-
-
-def extracting_the_dataset(df) -> object:
-    """
-    Оскільки назви деяких вулиць були змінені і колишні назви важливі, створює нову
-    колонку Past Names де будуть зберігатись колишні назви вулиць.
-    :param df: основна таблиця вулиць
-    :return: оновлена таблиця
-    """
-
-    df["Ex Names"] = df["Street"].str.extract("\((.*)\)")
-    df["Ex Names"] = df["Ex Names"].str.replace("бывш.", "")
-    df["Ex Names"].fillna("-", inplace=True)
-    df["Street"] = df["Street"].str.replace("\(бывш\..*\)", "")
-    return df
-
-
-def get_street_area(name, last_name, street, df):
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    area = df.loc[(df['Street'] == street) | (df['Ex Names'] == street), 'Area']
-    c.execute('''INSERT INTO streets (name, last_name, street, time) VALUES (?, ?, ?, ?)''',
-              (name, last_name, street, current_time))
-    conn.commit()
+def find_area(building_number, street_name, df):
+    area = df.loc[(df['Street'] == street_name) & (df['Number of Building'] == building_number), 'Area']
     return area
 
 
-if __name__ == "__main__":
-    name = 'Misha'
-    last_name = 'Dik'
-    street = '8 Марта улица'
-    print(get_street_area(name, last_name, street,
-                              extracting_the_dataset(convert_file_data_to_dataframe("kharkov_street.txt"))))
+def insert_data(name, last_name, building_number, street_name, df):
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    area = find_area(building_number, street_name, df)
+    unique_id = str(datetime.datetime.now().timestamp()).replace('.', '')[:8]
+    c.execute("INSERT INTO users2 VALUES (?, ?, ?, ?, ?, ?, ?)",
+              (unique_id, name, last_name, street_name, building_number, str(area), time))
+    conn.commit()
+
+
+insert_data('Misha', 'Dik', 'Білогорща вулиця', '98а', df)
